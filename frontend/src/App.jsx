0ccf4cc3e9c6715d96b0f65f528d3703a060fabb
Abhrajit Debnath <debnath.abhrajit2005@gmail.com>
2026-04-16 17:15:01 +0530

    feat: add route guards for authentication and authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,23 @@ import LoginPage from "./pages/Login.page";
 import DashboardPage from "./pages/Dashboard.page";
 import UserContextProvider from "./context/User.context";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute.component";
+import PublicRoute from "./components/PublicRoute.component";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/login", element: <LoginPage /> },
-    { path: "/dashboard", element: <DashboardPage /> },
+    {
+      path: "/login", element:
+        <PublicRoute>
+          <LoginPage />
+        </PublicRoute>
+    },
+    {
+      path: "/dashboard", element:
+        <ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>
+    },
   ]);
 
   return (
